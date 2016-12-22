26658ea3bf56c423ef637c967da27ba854c648df
Soline <Soline>
2016-12-22 11:07:12 +0100

    Noises are working and I started canal.m

--- canal.m
+++ canal.m
@@ -7,5 +7,12 @@ function signal_recu=canal(signal_module,h)
 % x_mod est le vecteur sorti du modulateur DMT.
 % h est la réponse impulsionnelle en temps du canal.
 
+signal_recu=signal_module;
 
-signal_recu=signal_module; 
+%We start with the convolution between the signal and h
+
+%We add noises
+signal_recu = add_awgn_noise(signal_recu);
+signal_recu = add_crosstalk(signal_recu, l, n);
+
+ 
