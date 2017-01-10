40a466558c69091f93e2dca88248b4c7eaade442
yutingWW <jaca.wang1994@gmail.com>
2017-01-10 18:01:58 +0100

    Add files via upload

--- ExampleTCA/demodulation_DMT.m
+++ ExampleTCA/demodulation_DMT.m
@@ -2,11 +2,11 @@ function [suite_bits] = demodulation_DMT( signal_receive, prefix_cyclic, allocat
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes her
    signal_receive = signal_receive(1+prefix_cyclic:512 + prefix_cyclic);
-   signal_receive_t = fft(signal_receive);
-   signal_reveive_t = signal_receive_t(1:256);
+   signal_receive_f = fft(signal_receive);
+   signal_reveive_f = signal_receive_f(1:256);
    for i=1:256
        if (allocation_table(i)~=0)
-           suite_bits = MQAMDemod(2^(allocation_table(i)),signal_receive_t);
+           suite_bits = MQAMDemod(2^(allocation_table(i)),signal_receive_f);
        end
    end
 
