0971a1a70dc8ab39b9f34d46fc015d9ad0140b9c
Soline <Soline>
2017-01-11 16:47:21 +0100

    maj bruit thermique

--- add_awgn_noise.m
+++ add_awgn_noise.m
@@ -7,16 +7,16 @@ k = 1.38*10^(-23); %Boltzmann
 T = 293.15; %Temperature in Kelvin
 B = 1.1*10^6; %Bandwidth in Hz 
 No = 10*log10(k*T*B); %Noise power
-Psig = 10*log10(bandpower(sig)); %Signal power
-n = 4; %Number of information bits per symbol OFDM
-snr = Psig/No + 10*log10(n);
+Psig = 10*log10(abs(mean(sig)).^2); %Signal power
+n = 544; %Number of information bits per symbol OFDM
+snr = Psig - No + 10*log10(n)
 
 %Create AWGN
 noise_therm=awgn(sig,snr);
 
 %Test
-figure(34)
-plot(scale_time,[sig,noise_therm])
+%figure(34)
+%plot(scale_time,[sig,noise_therm])
 
 end
 
