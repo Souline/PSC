af9dd25f164315dad8a78316a94e4f2a92b8f796
Shenzhi Zhang <shenzhizhang@Hotmail.com>
2017-01-15 18:12:09 +0800

    Modify_demodulationDMT.m
    Please read the comments and new code, thanks!

--- demodulationDMT.m
+++ demodulationDMT.m
@@ -1,8 +1,8 @@
 function [ supertrame_bin, x ] = demodulationDMT( signal_recu, h_eval_mod, tab )
 % inputs :
-% - signal_recu : signal reçu après passage dans le canal
-% - h_eval_mod : module de la réponse impulsionnelle du canal, identifiée
-% - nb_canaux : nombre de canaux utilisés
+% - signal_recu : signal reÃ§u aprÃ¨s passage dans le canal
+% - h_eval_mod : module de la rÃ©ponse impulsionnelle du canal, identifiÃ©e
+% - nb_canaux : nombre de canaux utilisÃ©s
 % - lgre_pref_cyclique : longueur du CP
 % - tab : vecteur table allocation des bits
 %
@@ -11,7 +11,7 @@ function [ supertrame_bin, x ] = demodulationDMT( signal_recu, h_eval_mod, tab )
 signal_recu = signal_recu/100;
 figure(70)
 plot(real(signal_recu));hold on;plot(imag(signal_recu), 'g');
-title('Entrée démodulateur');
+title('EntrÃ©e dÃ©modulateur');
 
 figure(700)
 plot(real(h_eval_mod));hold on;plot(imag(h_eval_mod), 'g');
@@ -25,20 +25,19 @@ x_freq = [];
 %signal_recu = signal_recu/P;
 
 for i=1:iteration
-    %% Suppression du préfixe cyclique
+    %% Suppression du prÃ©fixe cyclique
 
     trame = signal_recu(544*(i-1) + 33:i*544);
     n = 1;
 
-    %% FFT et égalisation du signal
+    %% FFT et Ã©galisation du signal
 
     x_fft = fft( trame );
-    x = x_fft( 1:256 );% suppression des coordonnées conjuguées introduites avant IFFT
-    x_freq = [x_freq x];
-    x = x./h_eval_mod; % égalisation
-    x_fin = [x_fin x];
+    x = x_fft( 1:256 );% suppression des coordonnÃ©es conjuguÃ©es introduites avant IFFT
+    x = round(x);
+    x = x./h_eval_mod; % Ã©galisation
 
-    %% Reconstruction des symboles et démodulation
+    %% Reconstruction des symboles et dÃ©modulation
 
     suite_bits = [];
     for j = 1:256
@@ -48,6 +47,12 @@ for i=1:iteration
             suite_bits = [suite_bits symb];
             n = n + 1;
         else
+            % please read repartitor.m, you can find that when a channel is allocated with 0 bit,
+            % the ofdm_symbol vector will have a zero at its corresponding position, therefore 
+            % after you do ifft and fft, there will be an entry in x vector which does not match 
+            % the original information bit. You need to set the pointer'n' + 1 in this situation as well
+            % to make the pointer has a correct position.
+            n = n + 1;
             continue;
         end
     end
@@ -58,10 +63,10 @@ end
 figure(212)
 subplot(211)
 plot(real(x_fin));hold on; plot(imag(x_fin), 'g');
-title('signal égalisé');
+title('signal Ã©galisÃ©');
 subplot(212)
 plot(real(x_freq));hold on;plot(imag(x_freq), 'g');
-title('Signal à la réception en fréquence');
+title('Signal Ã  la rÃ©ception en frÃ©quence');
 
 
 end
