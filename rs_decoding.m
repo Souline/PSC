d15853a2b4a71e6e8f0c3431fedda8e1ddc9e2c6
meijieli <meijie.li@insa-lyon.fr>
2017-01-17 14:49:31 +0100

    Update rs_decoding.m

--- rs_decoding.m
+++ rs_decoding.m
@@ -2,10 +2,10 @@ function rs_decoded = rs_decoding( msg, n, k )
 % RS ecoder
 %
 % this matlab script decodes the binary message in msg using an [ n, k ]
-% reed-solomon code with the narrow-sense polynomial
+% reed-solomon code with the narrow-sense polynomial generator
 % n is code word length and k is message word length
 % input is binary message
-% output is rs decoded reed integer values
+% output is rs decoded binary message
 %
 % example :
 % rs_decoded = rs_decoding( [ 0 1 0 1 1 1 0 0 1 1 0 0 0 1 0 ]', 5, 3 );
