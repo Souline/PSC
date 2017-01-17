45519bfa0295c9a82edaf6a5d25782c80650e3cb
meijieli <meijie.li@insa-lyon.fr>
2017-01-17 14:55:54 +0100

    Update interleaver.m

--- interleaver.m
+++ interleaver.m
@@ -13,7 +13,7 @@ function interleaved = interleaver( msg, nrows, slope )
 
 delay = nrows * ( nrows - 1 ) * slope;
 
-% append d symbols of delay to end of message
+% append the number of delay zeros to end of message
 msg = [ msg zeros( 1, delay ) ];
 
 % convolutional interleaver
